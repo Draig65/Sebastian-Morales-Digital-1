e03dcf030647f7b6ad572024ec0f3d130aae1b1e
daortizca <daortizca@unal.edu.co>
2026-05-09 17:45:42 -0500

    Update Corr_iz.v

--- Algoritmos/multiplicador/Core/Corr_iz.v
+++ Algoritmos/multiplicador/Core/Corr_iz.v
@@ -1,10 +1,10 @@
 module Corrimiento_iz(rst,clk,shift,load,b,out_b);
-  input rst
-  input clk,
-  input shift,
-  input load,
-  input [7:0] b,
-  output reg [31:0] out_b
+  input rst;
+  input clk;
+  input shift;
+  input load;
+  input [7:0] b;
+  output reg [31:0] out_b;
 
   always@(posedge clk) 
     if(rst)
