74515da1a11ceedb29126dadc187980d050b90a9
daortizca <daortizca@unal.edu.co>
2026-05-09 18:08:37 -0500

    Create Acc.v

---
+++ Algoritmos/multiplicador/Core/Acc.v
@@ -0,0 +1,18 @@
+module acumulador(rst,clk,b,add,out_r);
+  input rst;
+  input clk;
+  input [31:0] b;
+  input add;
+  output [31:0] out_r;
+
+  always @(negedge clk or posedge rst)
+    if(rst)
+      our_r<= 0;
+    else
+      begin
+        if(add)
+          out_r<=out_r +b;
+        else
+          out_r<=out_r;
+      end
+endmodule
