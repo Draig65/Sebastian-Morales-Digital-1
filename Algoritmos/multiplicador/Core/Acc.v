c26ba9871a32dee398c3ba516f585b09e05e0d36
daortizca <daortizca@unal.edu.co>
2026-05-19 07:42:03 -0500

    Update Acc.v

--- Algoritmos/multiplicador/Core/Acc.v
+++ Algoritmos/multiplicador/Core/Acc.v
@@ -1,14 +1,17 @@
-module acumulador(rst,clk,b,add,out_r);
+module acumulador(rst,clk,b,add,out_r,load);
   input rst;
   input clk;
   input [31:0] b;
   input add;
+  input load;
   output reg [31:0] out_r;
 
   always @(posedge clk or posedge rst)
     if(rst)
       out_r<= 0;
-    else
+  else if (load)
+    out_r <= 0;
+  else 
       begin
         if(add)
           out_r<=out_r +b;
@@ -16,3 +19,5 @@ module acumulador(rst,clk,b,add,out_r);
           out_r<=out_r;
       end
 endmodule
+
+
