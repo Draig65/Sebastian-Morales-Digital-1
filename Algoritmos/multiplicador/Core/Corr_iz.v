3f05855c2731b06adee1d604ce0e8453c7e2bc21
daortizca <daortizca@unal.edu.co>
2026-05-19 06:53:15 -0500

    Update Corr_iz.v

--- Algoritmos/multiplicador/Core/Corr_iz.v
+++ Algoritmos/multiplicador/Core/Corr_iz.v
@@ -8,12 +8,12 @@ module Corrimiento_iz(rst,clk,shift,load,b,out_b);
 
   always@(posedge clk) 
     if(rst)
-        b_out = 0 ;
+        out_b = 0 ;
     else
       begin
         if (load)
-          out_b=b;
-        else(shift)
-          out_b= b<<1;
+          out_b={16'b0, b};
+        else if (shift)
+          out_b= out_b<<1;
     end 
 endmodule 
