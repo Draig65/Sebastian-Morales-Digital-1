54b4bc253e2f08af953e7d9718d4120b3c2b6fd6
daortizca <daortizca@unal.edu.co>
2026-05-20 10:00:55 -0500

    Create Corr_c.v

---
+++ Algoritmos/Raiz_Cuadrada/Core/Corr_c.v
@@ -0,0 +1,21 @@
+module Corrimiento_c (
+    input            clk,
+    input            rst,
+    input            init,
+    input            load_b,
+    input      [4:0] B,
+    input            calc_c,
+    output reg [7:0] C
+);
+    always @(posedge clk or posedge rst) begin
+        if (rst || init) begin
+            C <= 8'b0;
+        end 
+      else begin
+            if (load_b) 
+                C <= {3'b0, B};          
+            else if (calc_c) 
+                C <= (C << 2) | 8'b1;    
+        end
+    end
+end
