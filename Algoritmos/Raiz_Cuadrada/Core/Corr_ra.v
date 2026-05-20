f1e3cd6961d833e385941825077728bdef42e86e
daortizca <daortizca@unal.edu.co>
2026-05-20 10:10:12 -0500

    Update Corr_ra.v

--- Algoritmos/Raiz_Cuadrada/Core/Corr_ra.v
+++ Algoritmos/Raiz_Cuadrada/Core/Corr_ra.v
@@ -1,15 +1,15 @@
-module Corrimiento_ra (
-    input            clk,
-    input            rst,
-    input            init,
-    input            load,
-    input      [7:0] radical_in,
-    input            shift,
-    input            sub_en,
-    input      [7:0] C,
-    output reg [7:0] A,
-    output reg [7:0] R
-);
+module Corrimiento_ra (clk,rst,init,load,radical_in, shift,sub_en,C,A,R);
+    input            clk;
+    input            rst;
+    input            init;
+    input            load;
+    input      [7:0] radical_in;
+    input            shift;
+    input            sub_en;
+    input      [7:0] C;
+    output reg [7:0] A;
+    output reg [7:0] R;
+
     always @(posedge clk or posedge rst) begin
         if (rst || init) begin
             A <= 8'b0;
