9321696098bfeb2f4b5baa2a8acbe6a636758bc1
daortizca <daortizca@unal.edu.co>
2026-05-20 09:58:54 -0500

    Create Corr_ra.v

---
+++ Algoritmos/Raiz_Cuadrada/Core/Corr_ra.v
@@ -0,0 +1,29 @@
+module Corrimiento_ra (
+    input            clk,
+    input            rst,
+    input            init,
+    input            load,
+    input      [7:0] radical_in,
+    input            shift,
+    input            sub_en,
+    input      [7:0] C,
+    output reg [7:0] A,
+    output reg [7:0] R
+);
+    always @(posedge clk or posedge rst) begin
+        if (rst || init) begin
+            A <= 8'b0;
+            R <= 8'b0;
+        end 
+      else if (load) begin
+            A <= radical_in;
+            R <= 8'b0;
+        end 
+      else begin
+            if (shift) 
+                {R, A} <= {R, A} << 2; 
+            else if (sub_en) 
+                R <= R - C;            // Bloque: R = R - C
+        end
+    end
+end
