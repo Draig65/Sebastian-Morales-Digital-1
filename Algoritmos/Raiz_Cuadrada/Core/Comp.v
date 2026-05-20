43e3bf445f6582db07930e0c7647e190dc7d4925
daortizca <daortizca@unal.edu.co>
2026-05-20 10:08:01 -0500

    Update Comp.v

--- Algoritmos/Raiz_Cuadrada/Core/Comp.v
+++ Algoritmos/Raiz_Cuadrada/Core/Comp.v
@@ -1,7 +1,7 @@
-module comparador (
-    input  [7:0] R,
-    input  [7:0] C,
-    output       z
-);
+module comparador (R,C,z);
+    input  [7:0] R;
+    input  [7:0] C;
+    output       z;
+    
     assign z = (R >= C);
 endmodule
