5b42f4d21d6661ec98c5f638c0b34865f66c1678
daortizca <daortizca@unal.edu.co>
2026-05-21 08:48:22 -0500

    Update raiz.v

--- Algoritmos/Raiz_Cuadrada/Core/raiz.v
+++ Algoritmos/Raiz_Cuadrada/Core/raiz.v
@@ -17,13 +17,9 @@ module raiz_cuadrada (
     assign residuo   = w_R;
 
     comparador comp   (.R(w_R), .C(w_C), .z(w_z));
-    
     Corrimiento_ra   corr_ra(.clk(clk), .rst(rst), .init(init), .load(w_load_ra),.radical_in(radical), .shift(w_shift_ra), .sub_en(w_sub_ra), .C(w_C), .A(w_A), .R(w_R));
-                       
-    corrimiento_b      corr_b(.clk(clk), .rst(rst), .init(init), .clear(w_clear_b),.shift(w_shift_b), .set_b0(w_set_b0), .B(w_B));
-                       
-    Corrimiento_c      corr_c (.clk(clk), .rst(rst), .init(init), .load_b(w_load_c),.B(w_B), .calc_c(w_calc_c), .C(w_C));
-                       
+    corrimiento_b      corr_b(.clk(clk), .rst(rst), .init(init), .clear(w_clear_b),.shift(w_shift_b), .set_b0(w_set_b0), .B(w_B));                
+    Corrimiento_c      corr_c (.clk(clk), .rst(rst), .init(init), .load_b(w_load_c),.B(w_B), .calc_c(w_calc_c), .C(w_C));                  
     control  ctrl(.clk(clk), .rst(rst), .init(init), .start(start), .z(w_z), .z_a(w_z_a), .load_ra(w_load_ra), .shift_ra(w_shift_ra), .sub_ra(w_sub_ra), 
                        .clear_b(w_clear_b), .shift_b(w_shift_b), .set_b0(w_set_b0), .load_c(w_load_c), .calc_c(w_calc_c), .done(done));
 endmodule
