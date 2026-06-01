a87eb90849c005a1c79d88e199fb827938b9bdaa
daortizca <daortizca@unal.edu.co>
2026-06-01 10:04:28 -0500

    Update Corr_c.v

--- Algoritmos/Raiz_Cuadrada/Core/Corr_c.v
+++ Algoritmos/Raiz_Cuadrada/Core/Corr_c.v
@@ -1,14 +1,13 @@
-module Corrimiento_c (clk,rst,init,load_b,B,calc_c,C);
+module Corrimiento_c (clk,rst,load_b,B,calc_c,C);
     input            clk;
     input            rst;
-    input            init;
     input            load_b;
     input      [4:0] B;
     input            calc_c;
     output reg [7:0] C;
     
     always @(posedge clk or posedge rst) begin
-        if (rst || init) begin
+        if (rst) begin
             C <= 8'b0;
         end 
       else begin
@@ -18,4 +17,4 @@ module Corrimiento_c (clk,rst,init,load_b,B,calc_c,C);
                 C <= (C << 2) | 8'b1;    
         end
     end
-end
+endmodule
