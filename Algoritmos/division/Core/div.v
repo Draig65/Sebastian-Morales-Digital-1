d64dbd51c8877297132c2f74599a8a7f069f9fd8
daortizca <daortizca@unal.edu.co>
2026-06-01 09:58:26 -0500

    Update div.v

--- Algoritmos/division/Core/div.v
+++ Algoritmos/division/Core/div.v
@@ -1,6 +1,5 @@
-module div_32 (clk, rst, init, start, A, B, cociente, resto, done);
-  input        clk, rst;
-  input        init;    
+module div_32 (clk, rst, start, A, B, cociente, done);
+  input        clk, rst;   
   input        start;   
   input  [15:0] A;      
   input  [15:0] B;      
@@ -19,12 +18,12 @@ module div_32 (clk, rst, init, start, A, B, cociente, resto, done);
   wire [31:0] w_acb;    
   wire [15:0] w_ac_sub; 
  
-  Corrimiento_acb    corr_acb (.clk(clk), .init(init),.in_B(B), .ac_sub(w_ac_sub),.shift(w_sh), .load(w_load), .load_ac(w_load_ac),.s_acb(w_acb), .msb(w_msb));
+  Corr_acb    corr_acb (.clk(clk), .rst(rst),.in_B(B), .ac_sub(w_ac_sub),.shift(w_sh), .load(w_load), .load_ac(w_load_ac),.s_acb(w_acb));
   resta_ac     dec (.Ac(w_acb[31:16]), .A(A), .ac_out(w_ac_sub));
-  corrimiento_r      corr_r   (.clk(clk), .init(init),.shift(w_sh), .load(w_load), .r0(w_r0),.s_r(cociente));
+  corrimiento_r      corr_r   (.clk(clk), .rst(rst),.shift(w_sh), .load(w_load), .r0(w_r0),.s_r(cociente));
   comparador_aca   comp(.Ac(w_acb[31:16]), .A(A), .z(w_z));
-  contador cnt      (.clk(clk), .init(init), .rst(rst),.load(w_load), .dec(w_dec), .z_co(w_z_co));
-  control ctrl    (.clk(clk), .rst(rst), .init(init),.start(start), .z(w_z), .z_co(w_z_co), .msb(w_msb),.done(done), .sh(w_sh), .load(w_load),.load_ac(w_load_ac), .r0(w_r0), .dec(w_dec));
+  contador cnt      (.clk(clk), .rst(rst),.load(w_load), .dec(w_dec), .z_co(w_z_co));
+  control ctrl    (.clk(clk), .rst(rst),.start(start), .z(w_z), .z_co(w_z_co),.done(done), .sh(w_sh), .load(w_load),.load_ac(w_load_ac), .r0(w_r0), .dec(w_dec));
   
   assign resto = w_acb[31:16];
 endmodule
