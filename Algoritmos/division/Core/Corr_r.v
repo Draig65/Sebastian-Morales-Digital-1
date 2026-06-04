eefac87eddc5d983a89e488541194a59795faf5a
daortizca <daortizca@unal.edu.co>
2026-06-03 21:10:46 -0500

    Update Corr_r.v

--- Algoritmos/division/Core/Corr_r.v
+++ Algoritmos/division/Core/Corr_r.v
@@ -8,12 +8,12 @@ module corrimiento_r (clk, rst, shift, load, r0, s_r);
  
   always @(negedge clk) begin
     if (rst)
-      s_r = 16'b0;
+      s_r <= 16'b0;
     else if (load)
-      s_r = 16'b0;
+      s_r <= 16'b0;
     else begin
-      if (shift) s_r = s_r << 1;
-      else if (r0)    s_r[0] = 1'b1;
+      if (shift) s_r <= s_r << 1;
+      else if (r0)    s_r[0] <= 1'b1;
     end
   end
 endmodule
