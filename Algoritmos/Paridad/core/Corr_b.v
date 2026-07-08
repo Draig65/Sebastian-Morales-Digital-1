578335ad71b998b23bbcf98e3503a74da6d57941
daortizca <daortizca@unal.edu.co>
2026-07-08 14:27:37 -0500

    Create Corr_b.v

---
+++ Algoritmos/Paridad/core/Corr_b.v
@@ -0,0 +1,19 @@
+module Corr_b (clk, rst, in_B, shift, load, out_B, b0);
+  input        clk;
+  input        rst;
+  input  [15:0] in_B;      
+  input        shift;      
+  input        load;       
+  output reg [15:0] out_B;   
+  output wire       b0;    
+
+  assign b0 = out_B[0];
+always @(posedge clk) begin
+    if (rst)
+        out_B<= 16'b0;
+    else if (load)
+      out_B <= in_B;
+  else if (shift)
+     out_B<= out_B>> 1;
+  end
+endmodule
