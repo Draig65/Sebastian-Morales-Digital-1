f7286006abec9c5b591858ad21b48ec78a108660
daortizca <daortizca@unal.edu.co>
2026-07-08 15:11:57 -0500

    Fix syntax error in paridad.v instantiation

--- Algoritmos/Paridad/core/paridad.v
+++ Algoritmos/Paridad/core/paridad.v
@@ -7,7 +7,7 @@ wire w_shift, w_load, w_inc, w_dec;
   wire w_z_co, w_b0;
   wire [4:0] w_r;
 
-   Corr_b Corr_b (.clk  (clk), .rst(rst), .in_B(B),.shift(w_shift), .load(w_load),.out_b (),.b0(w_b0))
+  Corr_b Corr_b (.clk  (clk), .rst(rst), .in_B(B),.shift(w_shift), .load(w_load),.out_b (),.b0(w_b0));
   cont   cont (.clk(clk), .rst(rst),.load(w_load), .dec(w_dec),.z_co(w_z_co));
   sum_r sum_r (.clk(clk), .rst(w_load), .inc(w_inc),.r(w_r));
   control control (.clk(clk), .rst(rst), .start(start),.b0(w_b0), .z_co(w_z_co),.done(done), .shift(w_shift),.load(w_load), .inc(w_inc),.dec(w_dec));
