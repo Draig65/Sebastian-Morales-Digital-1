47657e5c75f68d2b6ae97fb0cf5ac6788375a46c
daortizca <daortizca@unal.edu.co>
2026-07-08 10:56:29 -0500

    Update Corr_acb.v

--- Algoritmos/division/Core/Corr_acb.v
+++ Algoritmos/division/Core/Corr_acb.v
@@ -8,11 +8,11 @@ module Corrimiento_acb (clk, rst, in_B, ac_sub, shift, load, load_ac, s_acb);
   input        load_ac;   
   output reg [31:0] s_acb;
   
-  always @(negedge clk) begin
+  always @(posedge clk) begin
     if (rst)
       s_acb <= 32'b0;                       
     else if (load)
-      s_acb =< {16'b0, in_B};              
+      s_acb <= {16'b0, in_B};              
     else begin
       if (shift)   s_acb <= s_acb << 1;     
       if (load_ac) s_acb[31:16] <= ac_sub;  
