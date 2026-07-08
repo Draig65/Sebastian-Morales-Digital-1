4036a38ad58c23d6b76fc58b67f9df6342f180b4
daortizca <daortizca@unal.edu.co>
2026-07-08 10:54:21 -0500

    Update Corr_b.v

--- Algoritmos/Raiz_Cuadrada/Core/Corr_b.v
+++ Algoritmos/Raiz_Cuadrada/Core/Corr_b.v
@@ -1,17 +1,15 @@
 module Corr_b (clk,rst,shift,b0,B);
     input            clk;
     input            rst;
-    input            clear;
     input            shift;
     input            b0;
   output reg [4:0] B;
-  
-    always @(posedge clk or posedge rst) begin
-        if (rst) begin
+always @(posedge clk or posedge rst) begin
+if (rst) begin
             B <= 5'b0;
         end 
 else begin
-            if (shift)  B <= B << 1;   
+    if (shift)  B <= B << 1;   
             if (b0) B[0] <= 1'b1;  
         end
     end
