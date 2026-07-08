ad199b0fcea59911cd7f2f6cb230ca894052dcbc
daortizca <daortizca@unal.edu.co>
2026-07-08 10:57:36 -0500

    Update cont.v

--- Algoritmos/division/Core/cont.v
+++ Algoritmos/division/Core/cont.v
@@ -5,14 +5,14 @@ module contador(clk, rst, load, dec, z_co);
   input  dec;     
   output z_co;    
  
-  reg [4:0] count;
+reg [4:0] count;
  
   assign z_co = (count == 5'b0);
  
-  always @(negedge clk) begin
+  always @(posedge clk) begin
     if (rst)
       count <= 5'd16;         
-    else if (load)
+ else if (load)
       count <= 5'd16;         
     else if (dec)
       count <= count - 1'b1;  
