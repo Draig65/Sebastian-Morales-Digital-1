c8f17485ab9c56b273cb9eb7d221870ab07b82ba
daortizca <daortizca@unal.edu.co>
2026-07-08 19:03:14 -0500

    Update Corr_b.v
    Corrected input formatting and syntax issues in Corr_b.v.

--- Algoritmos/Paridad/core/Corr_b.v
+++ Algoritmos/Paridad/core/Corr_b.v
@@ -1,12 +1,11 @@
 module Corr_b (clk, rst, in_B, shift, load, out_B, b0);
   input        clk;
-  input        rst;
+input        rst;
   input  [15:0] in_B;      
   input        shift;      
-  input        load;       
+  input     load;       
   output reg [15:0] out_B;   
   output wire       b0;    
-
   assign b0 = out_B[0];
 always @(posedge clk) begin
     if (rst)
@@ -15,5 +14,5 @@ always @(posedge clk) begin
       out_B <= in_B;
   else if (shift)
      out_B<= out_B>> 1;
-  end
+end
 endmodule
