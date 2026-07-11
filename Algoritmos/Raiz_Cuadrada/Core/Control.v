304e3751c153ef4ae726f31218facc3cd9a5ba8b
daortizca <daortizca@unal.edu.co>
2026-07-11 13:29:28 -0500

    Update Control.v

--- Algoritmos/Raiz_Cuadrada/Core/Control.v
+++ Algoritmos/Raiz_Cuadrada/Core/Control.v
@@ -1,9 +1,9 @@
-module control(clk,rst,start,z,z_a,load_ra,shift_ra,sub_ra,clear_b,shift_b,set_b0,load_c,calc_c,done);
+module control(clk,rst,start,z,z_a,load_ra,shift_ra,sub_ra,shift_b,set_b0,load_c,calc_c,done);
     input      clk, rst, start;
     input      z;
     input      z_a;
     output reg load_ra, shift_ra, sub_ra;
-    output reg clear_b, shift_b, set_b0;
+    output reg shift_b, set_b0;
     output reg load_c, calc_c;
     output reg done;
   
@@ -11,63 +11,154 @@ module control(clk,rst,start,z,z_a,load_ra,shift_ra,sub_ra,clear_b,shift_b,set_b
     parameter SHIFT_RA  = 3'b001; 
     parameter SHIFT_CB  = 3'b010; 
     parameter CHECK = 3'b011; 
-    parameter CHECK_F  = 3'b100; 
-    parameter DONE     = 3'b101;
+    parameter DECB0 = 3'b100;
+    parameter CHECK_F  = 3'b101; 
+    parameter END     = 3'b111;
 
-    reg [2:0] state, next_state;
+reg [2:0] state;
+reg [3:0] count;
 
     always @(posedge clk or posedge rst) begin
-        if (rst) state <= START;
-        else  state <= next_state;
+        if (rst) begin
+        state <= START;
+        count <= 4'b0;
     end
-
-    // Transiciones de estado
-    always @(*) begin
-        next_state = state;
-        case (state)
-            START:    if (start) next_state = SHIFT_RA;
-            SHIFT_RA:   next_state = SHIFT_CB;
-            SHIFT_CB:   next_state = CHECK;
-            CHECK:    next_state = CHECK_F;
-            CHECK_F:   if (z_a)   next_state = DONE;
-                     else       next_state = SHIFT_RA;
-            DONE: if (rst)  next_state = START;
-            default: next_state = START;
-        endcase
-    end
-
-    // Generación de Salidas
-    always @(*) begin
-        load_ra = 0; shift_ra = 0; sub_ra = 0;
-        clear_b = 0; shift_b = 0;  set_b0 = 0;
-        load_c  = 0; calc_c = 0;   done = 0;
-
-        case (state)
+   else begin
+      case (state)
             START: begin
-                if (start) begin
-                    load_ra = 1;
-                    clear_b = 1;
-                end
+              if(start)
+                    state <= SHIFT_RA;
+                else 
+                    state <= START;
             end
-            SHIFT_RA: begin
-                shift_ra = 1;
-                load_c   = 1;
+
+            SHIFT_RA: 
+                  state <= SHIFT_CB;
+               
+
+            SHIFT_CB:
+                state <=CHECK;
+
+            CHECK:begin
+                if(z)
+                    state <= DECB0;
+                else
+                    state <= CHECK_F; 
             end
-            SHIFT_CB: begin
-                calc_c  = 1;
-                shift_b = 1;
+            DECB0:
+                state <=CHECK_F;
+
+            CHECK_F:begin
+                if(z_a)
+                    state <= END;
+                else
+                    state <= SHIFT_RA; 
             end
-            CHECK: begin
-                if (z) begin
-                    sub_ra = 1;
-                    set_b0 = 1;
-                end
+            END: begin
+                  if (start)
+                    state <= START;
+
+                else 
+                    state <= END;
             end
-            DONE: begin
-                done = 1;
-            end
-        endcase
+
+            default: state <= START;
+        endcase 
     end
+end
+   always @(*) begin
+    case (state)
+
+        START: begin
+            load_ra = 1;  
+        shift_ra = 0;
+        sub_ra = 0;
+        shift_b = 0;  
+        set_b0 = 0;
+        load_c  = 0;
+        calc_c = 0; 
+        done = 0;
+        end 
+
+        SHIFT_RA: begin
+            load_ra = 0;  
+        shift_ra = 1;
+        sub_ra = 0;
+        shift_b = 0;  
+        set_b0 = 0;
+        load_c  = 1;
+        calc_c = 0; 
+        done = 0;
+        end
+
+       SHIFT_CB: begin
+            load_ra = 0;  
+        shift_ra = 0;
+        sub_ra = 0;
+        shift_b = 1;  
+        set_b0 = 0;
+        load_c  = 0;
+        calc_c = 1; 
+        done = 0;
+        end
+
+        CHECK: begin
+            load_ra = 0;  
+        shift_ra = 0;
+        sub_ra = 0;
+        shift_b = 0;  
+        set_b0 = 0;
+        load_c  = 0;
+        calc_c = 0; 
+        done = 0;
+        end
+        DECB0: begin
+            load_ra = 0;  
+        shift_ra = 0;
+        sub_ra = 1;
+        shift_b = 0;  
+        set_b0 = 1;
+        load_c  = 0;
+        calc_c = 0; 
+        done = 0;
+        end
+
+        CHECK_F: begin
+            load_ra = 0;  
+        shift_ra = 0;
+        sub_ra = 0;
+        shift_b = 0;  
+        set_b0 = 0;
+        load_c  = 0;
+        calc_c = 0; 
+        done = 0;
+        end
+        END: begin
+        load_ra = 0;  
+        shift_ra = 0;
+        sub_ra = 0;
+        shift_b = 0;  
+        set_b0 = 0;
+        load_c  = 0;
+        calc_c = 0; 
+        done = 1;
+        end
+
+        default: begin
+         load_ra = 0;  
+        shift_ra = 0;
+        sub_ra = 0;
+        shift_b = 0;  
+        set_b0 = 0;
+        load_c  = 0;
+        calc_c = 0; 
+        done = 0;
+        end
+        
+    endcase
+end
+   
+
 `define BENCH
 `ifdef BENCH
   reg [8*40:1] state_name;
@@ -76,9 +167,10 @@ module control(clk,rst,start,z,z_a,load_ra,shift_ra,sub_ra,clear_b,shift_b,set_b
       START:      state_name = "START";
       SHIFT_RA: state_name = "SHIFT_RA";
       SHIFT_CB: state_name = "SHIFT_CB";
+      DECB0:       state_name = "DECB0";
       CHECK:       state_name = "CHECK";
       CHECK_F:     state_name = "CHK_CO";
-      DONE:     state_name = "END";
+      END:     state_name = "END";
     endcase
   end
 `endif
