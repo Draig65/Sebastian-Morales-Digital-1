0f8b96c8adbd9f66ab66952ca566b67280660728
daortizca <daortizca@unal.edu.co>
2026-07-11 15:46:52 -0500

    Rename output 'par' to 'resultado'

--- Algoritmos/Paridad/core/paridad.v
+++ Algoritmos/Paridad/core/paridad.v
@@ -1,7 +1,7 @@
 module paridad (clk, rst, start, B, par, done);
   input        clk, rst, start;
   input  [15:0] B;
- output         par;  
+ output         resultado;  
   output        done;  
 wire w_shift, w_load, w_inc, w_dec;
   wire w_z_co;
@@ -13,5 +13,5 @@ wire w_shift, w_load, w_inc, w_dec;
   cont   cont (.clk(clk), .rst(rst),.load(w_load), .dec(w_dec),.z_co(w_z_co));
   sum_r sum_r (.clk(clk), .rst(w_load), .inc(w_inc),.r(w_r));
   control control (.clk(clk), .rst(rst), .start(start),.b0(w_b0), .z_co(w_z_co),.done(done), .shift(w_shift),.load(w_load), .inc(w_inc),.dec(w_dec));
-  assign par = ~w_r[0];
+  assign resultado = ~w_r[0];
 endmodule
