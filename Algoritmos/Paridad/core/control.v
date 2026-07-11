5d30ab39fa98f684062c3394723529a5071edfc0
daortizca <daortizca@unal.edu.co>
2026-07-11 11:04:52 -0500

    Update control.v

--- Algoritmos/Paridad/core/control.v
+++ Algoritmos/Paridad/core/control.v
@@ -1,49 +1,163 @@
-module control (clk, rst, start, b0, z_co, done, shift, load, inc, dec);
-  input  clk, rst, start;
-  input  b0;          
+module control (clk, rst, init, b0, z_co, done, shift, load, inc, dec);
+  input  clk, rst, init;
+   input  b0;          
   input  z_co;        
 
-  output reg done; 
+   output reg done; 
   output reg shift;  
   output reg load;    
-  output reg inc;
+   output reg inc;
   output reg dec;     
 
   
-  parameter START  = 2'b00;
-   parameter LOAD   = 2'b01;
-   parameter SHIFT  = 2'b10;
-    parameter END_ST = 2'b11;
+  parameter START  = 3'b000;
+   parameter LOAD   = 3'b001;
+   parameter CHECK_B0= 3'b010;
+   parameter INC = 3'b011;
+   parameter DEC= 3'b100;
+    parameter SHIFT  = 3'b101;
+  parameter CHECK_Z = 3'b110;
+   parameter END_ST = 3'b111;
 
-  reg [1:0] state;
+  reg [2:0] state,next_state;
 
   always @(posedge clk or posedge rst) begin
-    if (rst)
-      state <= START;
-    else begin
-      case (state)
-        START : if (start) state <= LOAD;
-        LOAD  :           state <= SHIFT;
-        SHIFT : if (z_co) state <= END_ST;
-        END_ST: if (!start) state <= START;  
-      endcase
+    if (rst) begin
+        state <= START;
     end
-  end
+    else begin
+        case (state)
 
+            START: begin
+                if (init)
+                    state <= LOAD;
+                else
+                    state <= START;
+            end
 
-  always @(*) begin
+            LOAD: begin
+                state <= CHECK_B0;
+            end
 
-    {done, shift, load, inc, dec} = 5'b0;
+            CHECK_B0: begin
+                if (b0)
+                    state <= INC;
+                else
+                    state <= DEC;
+            end
+
+            INC: begin
+                state <= DEC;
+            end
+
+            DEC: begin
+                state <= SHIFT;
+            end
+
+            SHIFT: begin
+                state <= CHECK_Z;
+            end
+
+            CHECK_Z: begin
+                if (z_co)
+                    state <= END_ST;
+                else
+                    state <= CHECK_B0;
+            end
+
+            END_ST: begin
+                if (!init)
+                    state <= START;
+                else
+                    state <= END_ST;
+            end
+
+            default: begin
+                state <= START;
+            end
+
+        endcase
+    end
+end
+  
+
+always @(*) begin
     case (state)
-      LOAD : load      = 1'b1;
-      SHIFT: begin
-               shift     = 1'b1;
-               dec       = 1'b1;
-               inc  = b0;  
-             end
-      END_ST: done      = 1'b1;
+
+        START: begin
+            done  = 0;
+            shift = 0;
+            load  = 0;
+            inc   = 0;
+            dec   = 0;
+        end
+
+        LOAD: begin
+            done  = 0;
+            shift = 0;
+            load  = 1;
+            inc   = 0;
+            dec   = 0;
+        end
+
+        CHECK_B0: begin
+            done  = 0;
+            shift = 0;
+            load  = 0;
+            inc   = 0;
+            dec   = 0;
+        end
+
+        INC: begin
+            done  = 0;
+            shift = 0;
+            load  = 0;
+            inc   = 1;
+            dec   = 0;
+        end
+
+        DEC: begin
+            done  = 0;
+            shift = 0;
+            load  = 0;
+            inc   = 0;
+            dec   = 1;
+        end
+
+        SHIFT: begin
+            done  = 0;
+            shift = 1;
+            load  = 0;
+            inc   = 0;
+            dec   = 0;
+        end
+
+        CHECK_Z: begin
+            done  = 0;
+            shift = 0;
+            load  = 0;
+            inc   = 0;
+            dec   = 0;
+        end
+
+        END_ST: begin
+            done  = 1;
+            shift = 0;
+            load  = 0;
+            inc   = 0;
+            dec   = 0;
+        end
+
+        default: begin
+            done  = 0;
+            shift = 0;
+            load  = 0;
+            inc   = 0;
+            dec   = 0;
+        end
+
     endcase
-  end
+end
 
 `define BENCH
 `ifdef BENCH
@@ -52,7 +166,11 @@ module control (clk, rst, start, b0, z_co, done, shift, load, inc, dec);
     case (state)
       START : state_name = "START";
       LOAD  : state_name = "LOAD";
+      CHECK_B0: state_name = "CHECK_B0";
+      INC : state_name = "INC";
+      DEC : state_name = "DEC";
       SHIFT : state_name = "SHIFT";
+      CHECK_Z: state_name = "CHECK_Z";
       END_ST: state_name = "END";
       default: state_name = "UNKN";
     endcase
