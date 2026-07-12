b6b9af7e814d5a39dab95b50134a0e80e8b666f5
daortizca <daortizca@unal.edu.co>
2026-07-11 22:13:25 -0500

    Refactor paridad module and update signal names

--- Algoritmos/Paridad/core/paridad.v
+++ Algoritmos/Paridad/core/paridad.v
@@ -1,4 +1,4 @@
-module paridad (clk, rst, start, B, par, done);
+module paridad (clk, rst, start, B, resultado, done);
   input        clk, rst, start;
   input  [15:0] B;
  output         resultado;  
@@ -9,9 +9,10 @@ wire w_shift, w_load, w_inc, w_dec;
   wire [15:0] w_out_b;
   wire        w_b0;
 
-  Corr_b Corr_b (.clk  (clk), .rst(rst), .in_B(B),.shift(w_shift), .load(w_load),.out_b(w_out_b),.b0(w_b0));
+  Corr_b Corr_b (.clk  (clk), .rst(rst), .in_B(B),.shift(w_shift), .load(w_load),.out_B(w_out_b),.b0(w_b0));
   cont   cont (.clk(clk), .rst(rst),.load(w_load), .dec(w_dec),.z_co(w_z_co));
-  sum_r sum_r (.clk(clk), .rst(w_load), .inc(w_inc),.r(w_r));
-  control control (.clk(clk), .rst(rst), .start(start),.b0(w_b0), .z_co(w_z_co),.done(done), .shift(w_shift),.load(w_load), .inc(w_inc),.dec(w_dec));
-  assign resultado = ~w_r[0];
+  sum_r sum_r (.clk(clk), .load(w_load), .inc(w_inc),.r(w_r),.rst(rst));
+  
+  control control (.clk(clk), .rst(rst), .init(start),.b0(w_b0), .z_co(w_z_co),.done(done), .shift(w_shift),.load(w_load), .inc(w_inc),.dec(w_dec));
+  assign resultado = w_r[0];
 endmodule
